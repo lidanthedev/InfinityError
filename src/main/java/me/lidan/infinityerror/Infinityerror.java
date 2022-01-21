18cb905580cb5c62368e92f6afe4244f9a35beba
Lidan <ls301205@gmail.com>
2022-01-21 17:16:29 +0200

    Added skull to the infinity gauntlet

--- src/main/java/me/lidan/infinityerror/Infinityerror.java
+++ src/main/java/me/lidan/infinityerror/Infinityerror.java
@@ -8,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -15,7 +16,7 @@ public final class Infinityerror extends JavaPlugin {
 
     public static Logger LOGGER = LogManager.getLogger();
     private static Infinityerror instance = null;
-    public static HashMap<Player, Ability[]> playerAbilityMap = new HashMap<Player, Ability[]>();
+    public static HashMap<Player, ArrayList<Ability>> playerAbilityMap = new HashMap<>();
 
     @Override
     public void onEnable() {
