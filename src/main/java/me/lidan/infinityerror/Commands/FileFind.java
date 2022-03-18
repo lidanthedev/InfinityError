231c2e55a368a1465f6191522073206565e16a1e
Lidan <ls301205@gmail.com>
2022-03-18 18:22:51 +0200

    Added folder flag

--- src/main/java/me/lidan/infinityerror/Commands/FileFind.java
+++ src/main/java/me/lidan/infinityerror/Commands/FileFind.java
@@ -2,7 +2,6 @@ package me.lidan.infinityerror.Commands;
 
 import me.lidan.infinityerror.Infinityerror;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,10 +10,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 public class FileFind implements CommandExecutor {
     public JavaPlugin plugin = Infinityerror.getInstance();
-    public static Logger LOGGER = LogManager.getLogger(FileFind.class);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -25,10 +25,10 @@ public class FileFind implements CommandExecutor {
     }
 
     public void searchFiles(CommandSender sender, String search) {
-        // flag --lower
+        // Flags
         boolean lower = false;
         String folderPath = "";
-        if (search.contains(" --folder")){
+        if (search.contains(" --folder")){ // Folder flag
             String[] splitSearch = search.split(" ");
             boolean foundFolderFlag = false;
             for (String s : splitSearch) {
@@ -42,35 +42,50 @@ public class FileFind implements CommandExecutor {
             search = search.replace(" --folder", "");
             search = search.replace(String.format(" %s", folderPath), "");
         }
-        if (search.contains(" --lower")){
-            search = search.replace(" --lower","");
-            lower = true;
-        }
         else {
             folderPath = plugin.getDataFolder().getParent() + "/Skript" +
                     "/scripts";
         }
-        if (search.contains("--")){
+        if (search.contains(" --lower")){ // Lower flag
+            search = search.replace(" --lower","");
+            search = search.toLowerCase();
+            lower = true;
+        }
+
+        if (search.contains("--")){ // Not Implemented flag
             sender.sendMessage("[FileFind] WARN found unsolved flags!");
         }
+
         final File folder = new File(folderPath);
+        String finalSearch = search;
+        boolean finalLower = lower;
+        searchFiles(sender, folder, finalSearch, finalLower);
+    }
+
+    public void searchFiles(CommandSender sender, File folder, String finalSearch, boolean finalLower) {
         if (!folder.exists() || !folder.isDirectory()){
-            sender.sendMessage(String.format("Folder %s not found", folder.getPath()));
+            sender.sendMessage(String.format("Folder %s not", folder.getPath()));
             return;
         }
-        String finalSearch = search;
+
         new BukkitRunnable() {
             @Override
             public void run() {
+
                 try {
+                    Logger logger = Logger.getLogger("FileFindLog");
+                    FileHandler fh = new FileHandler("");
                     sender.sendMessage(String.format("[FileFind] Searching for %s in folder %s", finalSearch, folder.getPath()));
                     int count = 0;
-                    for (final File fileEntry : folder.listFiles()) {
+                    File[] files = folder.listFiles();
+                    assert files != null;
+                    for (final File fileEntry : files) {
                         // LOGGER.info("now reading file {}", fileEntry.getName());
                         BufferedReader reader = new BufferedReader(new FileReader(fileEntry.getAbsolutePath()));
                         int lines = 1;
                         String s;
                         while ((s = reader.readLine()) != null) {
+                            if (finalLower) s = s.toLowerCase();
                             if (s.contains(finalSearch)) {
                                 s = s.replace(finalSearch, ChatColor.RED + finalSearch + ChatColor.RESET);
                                 sender.sendMessage("[FileFind] " + fileEntry.getName() + " " + s + " at line " + lines);
